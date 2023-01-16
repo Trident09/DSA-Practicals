f4fa256ee1b33f3ac525cf65925ec734e0ac8829
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-01-16 23:08:04 +0530

    Update and rename Selection sort.c to Selection sort.cpp

--- Selection sort.cpp
+++ Selection sort.cpp
@@ -1,6 +1,9 @@
-// C program for implementation of selection sort
-#include <stdio.h>
+// C++ program for implementation of
+// selection sort
+#include <bits/stdc++.h>
+using namespace std;
 
+//Swap function
 void swap(int *xp, int *yp)
 {
 	int temp = *xp;
@@ -12,28 +15,32 @@ void selectionSort(int arr[], int n)
 {
 	int i, j, min_idx;
 
-	// One by one move boundary of unsorted subarray
+	// One by one move boundary of
+	// unsorted subarray
 	for (i = 0; i < n-1; i++)
 	{
-		// Find the minimum element in unsorted array
+	
+		// Find the minimum element in
+		// unsorted array
 		min_idx = i;
 		for (j = i+1; j < n; j++)
 		if (arr[j] < arr[min_idx])
 			min_idx = j;
 
-		// Swap the found minimum element with the first element
-		if(min_idx != i)
+		// Swap the found minimum element
+		// with the first element
+		if(min_idx!=i)
 			swap(&arr[min_idx], &arr[i]);
 	}
 }
 
-/* Function to print an array */
+//Function to print an array
 void printArray(int arr[], int size)
 {
 	int i;
 	for (i=0; i < size; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
 // Driver program to test above functions
@@ -42,7 +49,7 @@ int main()
 	int arr[] = {64, 25, 12, 22, 11};
 	int n = sizeof(arr)/sizeof(arr[0]);
 	selectionSort(arr, n);
-	printf("Sorted array: \n");
+	cout << "Sorted array: \n";
 	printArray(arr, n);
 	return 0;
 }
