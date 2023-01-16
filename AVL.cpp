60abf5d52967593e09497e2da83fa21c8a82275e
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-01-16 22:19:08 +0530

    Update AVL.cpp

--- AVL.cpp
+++ AVL.cpp
@@ -13,116 +13,141 @@
 
 #include <iostream>
 
-class Node {
+class Node
+{
 public:
     int data;
     int height;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    Node(int data) {
+    Node(int data)
+    {
         this->data = data;
         this->height = 1;
         this->left = this->right = nullptr;
     }
 };
 
-class AVLTree {
+class AVLTree
+{
 public:
-    Node* root;
+    Node *root;
 
-    AVLTree() {
+    AVLTree()
+    {
         this->root = nullptr;
     }
 
-    int height(Node* node) {
-        if (!node) {
+    int height(Node *node)
+    {
+        if (!node)
+        {
             return 0;
         }
         return node->height;
     }
 
-    int getBalance(Node* node) {
-        if (!node) {
+    int getBalance(Node *node)
+    {
+        if (!node)
+        {
             return 0;
         }
         return height(node->left) - height(node->right);
     }
 
-    Node* rightRotate(Node* y) {
-        Node* x = y->left;
-        Node* T2 = x->right;
+    Node *rightRotate(Node *y)
+    {
+        Node *x = y->left;
+        Node *T2 = x->right;
         x->right = y;
         y->left = T2;
         y->height = 1 + std::max(height(y->left), height(y->right));
         x->height = 1 + std::max(height(x->left), height(x->right));
         return x;
     }
-    Node* leftRotate(Node* x) {
-    Node* y = x->right;
-    Node* T2 = y->left;
-    y->left = x;
-    x->right = T2;
-    x->height = 1 + std::max(height(x->left), height(x->right));
-    y->height = 1 + std::max(height(y->left), height(y->right));
-    return y;
-}
-
-Node* insert(Node* node, int data) {
-    if (!node) {
-        return new Node(data);
+    Node *leftRotate(Node *x)
+    {
+        Node *y = x->right;
+        Node *T2 = y->left;
+        y->left = x;
+        x->right = T2;
+        x->height = 1 + std::max(height(x->left), height(x->right));
+        y->height = 1 + std::max(height(y->left), height(y->right));
+        return y;
     }
-    if (data < node->data) {
-        node->left = insert(node->left, data);
-    } else if (data > node->data) {
-        node->right = insert(node->right, data);
-    } else {
+
+    Node *insert(Node *node, int data)
+    {
+        if (!node)
+        {
+            return new Node(data);
+        }
+        if (data < node->data)
+        {
+            node->left = insert(node->left, data);
+        }
+        else if (data > node->data)
+        {
+            node->right = insert(node->right, data);
+        }
+        else
+        {
+            return node;
+        }
+        node->height = 1 + std::max(height(node->left), height(node->right));
+        int balance = getBalance(node);
+        if (balance > 1 && data < node->left->data)
+        {
+            return rightRotate(node);
+        }
+        if (balance < -1 && data > node->right->data)
+        {
+            return leftRotate(node);
+        }
+        if (balance > 1 && data > node->left->data)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+        if (balance < -1 && data < node->right->data)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
         return node;
     }
-    node->height = 1 + std::max(height(node->left), height(node->right));
-    int balance = getBalance(node);
-    if (balance > 1 && data < node->left->data) {
-        return rightRotate(node);
-    }
-    if (balance < -1 && data > node->right->data) {
-        return leftRotate(node);
-    }
-    if (balance > 1 && data > node->left->data) {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
-    }
-    if (balance < -1 && data < node->right->data) {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
-    return node;
-}
 
-void inOrder(Node* root) {
-    if (root != nullptr) {
-        inOrder(root->left);
-        std::cout << root->data << " ";
-        inOrder(root->right);
+    void inOrder(Node *root)
+    {
+        if (root != nullptr)
+        {
+            inOrder(root->left);
+            std::cout << root->data << " ";
+            inOrder(root->right);
+        }
     }
-}
 
-void insert(int data) {
-    root = insert(root, data);
-}
+    void insert(int data)
+    {
+        root = insert(root, data);
+    }
 };
 
-int main() {
-AVLTree tree;
-tree.insert(9);
-tree.insert(5);
-tree.insert(10);
-tree.insert(0);
-tree.insert(6);
-tree.insert(11);
-tree.insert(-1);
-tree.insert(1);
-tree.insert(2);
-std::cout << "In-order traversal of the tree: ";
-tree.inOrder(tree.root);
-return 0;
+int main()
+{
+    AVLTree tree;
+    tree.insert(9);
+    tree.insert(5);
+    tree.insert(10);
+    tree.insert(0);
+    tree.insert(6);
+    tree.insert(11);
+    tree.insert(-1);
+    tree.insert(1);
+    tree.insert(2);
+    std::cout << "In-order traversal of the tree: ";
+    tree.inOrder(tree.root);
+    return 0;
 }
