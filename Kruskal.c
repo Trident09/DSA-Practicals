848b19f5cce21eb8c39febba224f5e40ca8964bc
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-01-16 21:16:53 +0530

    Update Kruskal.c

--- Kruskal.c
+++ Kruskal.c
@@ -7,100 +7,111 @@
       b. If the edge connects two vertices that are already in the same component, ignore it
     Return the minimum spanning tree */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX_VERTICES 100
-#define MAX_EDGES 10000
+int comparator(const void *p1,const void *p2)//used by qsort()
+{
+	const int (*x)[3]=p1;
+	const int (*y)[3]=p2;
 
-// edge structure
-struct Edge {
-    int src, dest, weight;
-};
-
-// structure for graph
-struct Graph {
-    int V, E;
-    struct Edge* edge;
-};
-
-// create a graph
-struct Graph* createGraph(int V, int E) {
-    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
-    graph->V = V;
-    graph->E = E;
-    graph->edge = (struct Edge*) malloc(E * sizeof(struct Edge));
-    return graph;
+	return (*x)[2]-(*y)[2];
 }
 
-// find set of an element i
-int find(int parent[], int i) {
-    if (parent[i] == -1)
-        return i;
-    return find(parent, parent[i]);
+void makeSet(int parent[],int rank[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		parent[i]=i;
+		rank[i]=0;
+	}
 }
 
-// union of two sets
-void Union(int parent[], int x, int y) {
-    int xset = find(parent, x);
-    int yset = find(parent, y);
-    if (xset != yset)
-        parent[xset] = yset;
+int findParent(int parent[],int component)
+{
+	if(parent[component]==component)
+	return component;
+
+	return parent[component]=findParent(parent,parent[component]);
 }
 
-// compare two edges according to their weights
-int cmp(const void* a, const void* b) {
-    struct Edge* a1 = (struct Edge*) a;
-    struct Edge* b1 = (struct Edge*) b;
-    return a1->weight > b1->weight;
+void unionSet(int u,int v,int parent[],int rank[],int n)
+{
+	//this function unions two set on the basis of rank
+	//as shown below
+	u=findParent(parent,u);
+	v=findParent(parent,v);
+
+	if(rank[u]<rank[v])
+	{
+		parent[u]=v;
+	}
+	else if(rank[u]<rank[v])
+	{
+		parent[v]=u;
+	}
+	else
+	{
+		parent[v]=u;
+		rank[u]++;//since the rank increases if the ranks of two sets are same
+	}
 }
 
-// Kruskal's algorithm
-void Kruskal(struct Graph* graph) {
-    int V = graph->V;
-    struct Edge result[V];
-    int e = 0;
-    int i = 0;
-    qsort(graph->edge, graph->E, sizeof(graph->edge[0]), cmp);
-    int *parent = (int*) malloc(V * sizeof(int));
-    memset(parent, -1, sizeof(parent));
-    while (e < V - 1) {
-        struct Edge next_edge = graph->edge[i++];
-        int x = find(parent, next_edge.src);
-        int y = find(parent, next_edge.dest);
-        if (x != y) {
-            result[e++] = next_edge;
-            Union(parent, x, y);
-        }
-    }
-    // printing the minimum spanning tree
-    printf("Minimum Spanning Tree:\n");
-    for (i = 0; i < e; ++i) {
-        printf("%d - %d : %d\n", result[i].src, result[i].dest, result[i].weight);
-    }
+void kruskalAlgo(int n,int edge[n][3])
+{
+	//First we sort the edge array in ascending order
+	//so that we can access minimum distances/cost
+	qsort(edge,n,sizeof(edge[0]),comparator);
+	//inbuilt quick sort function comes with stdlib.h
+	//go to https://www.geeksforgeeks.org/comparator-function-of-qsort-in-c/
+	//if there is any doubt regarding the function
+
+	int parent[n];
+	int rank[n];
+	makeSet(parent,rank,n);//function to initialize parent[] and rank[]
+
+	int minCost=0;//to store the minimun cost
+
+	printf("Following are the edges in the constructed MST\n");
+	for(int i=0;i<n;i++)
+	{
+		int v1=findParent(parent,edge[i][0]);
+		int v2=findParent(parent,edge[i][1]);
+		int wt=edge[i][2];
+
+		if(v1!=v2)//if the parents are different that means they are in
+				//different sets so union them
+		{
+			unionSet(v1,v2,parent,rank,n);
+			minCost+=wt;
+			printf("%d -- %d == %d\n",edge[i][0],edge[i][1],wt);
+		}
+	}
+
+	printf("Minimum Cost Spanning Tree: %d\n",minCost);
 }
 
-int main() {
-    int V = 4;
-    int E = 5;
-    struct Graph* graph = createGraph(V, E);
-    graph->edge[0].src = 0;
-    graph->edge[0].dest = 1;
-    graph->edge[0].weight = 10;
-    graph->edge[1].src = 0;
-    graph->edge[1].dest = 2;
-    graph->edge[1].weight = 6;
-    graph->edge[2].src = 0;
-    graph->edge[2].dest = 3;
-    graph->edge[2].weight = 5;
-    graph->edge[3].src = 1;
-    graph->edge[3].dest = 3;
-    graph->edge[3].weight = 15;
-    graph->edge[4].src = 2;
-    graph->edge[4].dest = 3;
-    graph->edge[4].weight = 4;
-    Kruskal(graph);
-    return 0;
+int main()
+{
+
+	/* Let us create following weighted graph
+				10
+			0--------1
+			| \	 |
+			6| 5\ |15
+			|	 \ |
+			2--------3
+				4	 */
+
+	//Here 5 is the number of edges, can be asked from the user
+	//when making the graph through user input
+	//3 represents the no of index positions for storing u --> v(adjacent vertices)
+	//and its cost/distance;
+	int edge[5][3]={{0,1,10},
+					{0,2,6},
+					{0,3,5},
+					{1,3,15},
+					{2,3,4}};
+
+	kruskalAlgo(5,edge);
 }
