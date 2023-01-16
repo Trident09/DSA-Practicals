95b29f98a4922b9d6c4846504b9396690780171d
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-01-16 21:20:03 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -6,84 +6,95 @@
       b. If the vertex has not been visited, mark it as visited and add it to the set or array.
       c. Enqueue all of the vertex's unvisited neighbors. */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VERTICES 100
+#define MAX_VERTICES 50
 
-// adjacency list node
-struct AdjListNode {
-    int dest;
-    struct AdjListNode* next;
-};
+// This struct represents a directed graph using
+// adjacency list representation
+typedef struct Graph_t {
+	int V; // No. of vertices
+	bool adj[MAX_VERTICES][MAX_VERTICES];
+} Graph;
 
-// adjacency list
-struct AdjList {
-    struct AdjListNode* head;
-};
+// Constructor
+Graph* Graph_create(int V)
+{
+	Graph* g = malloc(sizeof(Graph));
+	g->V = V;
 
-// graph
-struct Graph {
-    int V;
-    struct AdjList* array;
-};
+	for (int i = 0; i < V; i++) {
+		for (int j = 0; j < V; j++) {
+			g->adj[i][j] = false;
+		}
+	}
 
-// create a new adjacency list node
-struct AdjListNode* newAdjListNode(int dest) {
-    struct AdjListNode* newNode = (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
-    newNode->dest = dest;
-    newNode->next = NULL;
-    return newNode;
+	return g;
 }
 
-// create a graph of V vertices
-struct Graph* createGraph(int V) {
-    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
-    graph->V = V;
-    graph->array = (struct AdjList*) malloc(V * sizeof(struct AdjList));
-    int i;
-    for (i = 0; i < V; i++)
-        graph->array[i].head = NULL;
-    return graph;
+// Destructor
+void Graph_destroy(Graph* g) { free(g); }
+
+// function to add an edge to graph
+void Graph_addEdge(Graph* g, int v, int w)
+{
+	g->adj[v][w] = true; // Add w to v’s list.
 }
 
-// add an edge to a directed graph
-void addEdge(struct Graph* graph, int src, int dest) {
-    struct AdjListNode* newNode = newAdjListNode(dest);
-    newNode->next = graph->array[src].head;
-    graph->array[src].head = newNode;
+// prints BFS traversal from a given source s
+void Graph_BFS(Graph* g, int s)
+{
+	// Mark all the vertices as not visited
+	bool visited[MAX_VERTICES];
+	for (int i = 0; i < g->V; i++) {
+		visited[i] = false;
+	}
+
+	// Create a queue for BFS
+	int queue[MAX_VERTICES];
+	int front = 0, rear = 0;
+
+	// Mark the current node as visited and enqueue it
+	visited[s] = true;
+	queue[rear++] = s;
+
+	while (front != rear) {
+		// Dequeue a vertex from queue and print it
+		s = queue[front++];
+		printf("%d ", s);
+
+		// Get all adjacent vertices of the dequeued
+		// vertex s. If a adjacent has not been visited,
+		// then mark it visited and enqueue it
+		for (int adjecent = 0; adjecent < g->V;
+			adjecent++) {
+			if (g->adj[s][adjecent] && !visited[adjecent]) {
+				visited[adjecent] = true;
+				queue[rear++] = adjecent;
+			}
+		}
+	}
 }
 
-// breadth-first search
-void bfs(struct Graph* graph, int start) {
-    int* visited = (int*) malloc(graph->V * sizeof(int));
-    int i;
-    for (i = 0; i < graph->V; i++)
-        visited[i] = 0;
-    visited[start] = 1;
-    printf("%d ", start);
-    struct AdjListNode* temp = graph->array[start].head;
-    while (temp) {
-        int v = temp->dest;
-        if (!visited[v]) {
-            printf("%d ", v);
-            visited[v] = 1;
-        }
-        temp = temp->next;
-    }
-    printf("\n");
-}
+// Driver program to test methods of graph struct
+int main()
+{
+	// Create a graph given in the above diagram
+	Graph* g = Graph_create(4);
+	Graph_addEdge(g, 0, 1);
+	Graph_addEdge(g, 0, 2);
+	Graph_addEdge(g, 1, 2);
+	Graph_addEdge(g, 2, 0);
+	Graph_addEdge(g, 2, 3);
+	Graph_addEdge(g, 3, 3);
 
-int main() {
-    int V = 5;
-    struct Graph* graph = createGraph(V);
-    addEdge(graph, 0, 1);
-    addEdge(graph, 0, 4);
-    addEdge(graph, 1, 2);
-    addEdge(graph, 1, 3);
-    addEdge(graph, 1, 4);
-    addEdge(graph, 2, 3);
-    addEdge(graph, 3, 4);
-    bfs(graph, 0);
-    return 0;
+	printf("Following is Breadth First Traversal "
+		"(starting from vertex 2) \n");
+	Graph_BFS(g, 2);
+
+	Graph_destroy(g);
+
+	return 0;
 }
